fb00566a8111452a6582755b0c24f71d5669f0a9
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-02 20:52:09 +0530

    Initial Commit

---
+++ WhatsApp-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsApp-Clone
+//
+//  Created by Anshumali Karna on 02/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsApp-Clone/WhatsApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CloneApp.swift
+//  WhatsApp-Clone
+//
+//  Created by Anshumali Karna on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
